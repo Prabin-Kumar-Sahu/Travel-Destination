42525bd98d6ede8b70c48722d73fe6c71e9ac488
Prabin Kumar Sahu <78588455+Prabin-Kumar-Sahu@users.noreply.github.com>
2023-08-01 12:38:11 +0530

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import video2 from "../../Assets/video2.mp4";
+import { BiSend } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div></div>
-  )
-}
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
 
-export default Footer
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel With Us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="enter your mail address" />
+            <button className="btn flex" type="submit">
+              SEND <BiSend className="icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
